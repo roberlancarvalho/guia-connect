76afabb8931f4a314ecd47481aeba93bb445177f
Roberlan Carvalho <roberlan.carvalho@gmail.com>
2021-04-29 23:22:07 -0300

    Consertando os imports em charts.module.ts

--- src/app/layout/graficos/charts.module.ts
+++ src/app/layout/graficos/charts.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsModule as Ng2Charts } from 'ng2-charts';
 
-import { ChartsRoutingModule } from './charts-routing.module';
-import { ChartsComponent } from './charts.component';
+
 import { PageHeaderModule } from '../../shared';
+import { ChartsRoutingModule } from '../charts/charts-routing.module';
+import { ChartsComponent } from '../charts/charts.component';
 
 @NgModule({
     imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule],
